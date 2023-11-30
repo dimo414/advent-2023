bd72cc920a6ed071d72721c5da4a6f0bd6d86ef7
Michael Diamond <dimo414@gmail.com>
2023-11-30 11:34:47 -0800

    Initialize 2023 repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Solutions for Advent of Code 2023
+
+## Timings
+
+See the "Timing" step in the [CI action](https://github.com/dimo414/advent-2023/actions)
+for some basic benchmarks.
---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+extern crate lazy_static;
+extern crate regex;
+extern crate anyhow;
+
+pub mod collect;
+pub mod euclid3d;
+pub mod euclid;
+pub mod parsing;
+pub mod pathfinding;
+pub mod terminal;
